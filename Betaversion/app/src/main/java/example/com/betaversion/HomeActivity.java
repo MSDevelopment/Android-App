f444b9f098b375d2d305c77392e3c32483b89a95
MSDevelopment <mg.sp.development@gmail.com>
2018-07-26 15:22:23 -0400

    Corrected to reflect the correct AS folder
    - Pushed the wrong Android Studio folder (This commit has the correct folder/project)

--- Betaversion/app/src/main/java/example/com/betaversion/HomeActivity.java
+++ Betaversion/app/src/main/java/example/com/betaversion/HomeActivity.java
@@ -2,35 +2,20 @@ package example.com.betaversion;
 
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.net.Uri;
 import android.os.BatteryManager;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
 
 public class HomeActivity extends AppCompatActivity {
-    private ImageView iv_battery;
-    private TextView tv_battery;
-    private Handler handler;
-    private Runnable runnable;
+
+    ImageView iv_battery;
+    TextView tv_battery;
+
+    Handler handler;
+    Runnable runnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,20 +58,6 @@ public class HomeActivity extends AppCompatActivity {
 
         handler = new Handler();
         handler.postDelayed(runnable, 0);
-
-        public void insertDb (View insertBtn) {
-            // Create the references
-            TextView textView = (TextView) findViewById(R.id.tv_battery);
-
-            // Instantiate the book object
-            Book bookObj = new Book(textView.getText().toString());
-
-            // Call the bookhelper's add method
-            BookHelper helper = new BookHelper(this);
-            helper.addBook(bookObj);
-
-            Toast.makeText(this, "Record insert successfully", Toast.LENGTH_SHORT).show();
-        }
     }
 
     public float batteryLevel(){
@@ -99,5 +70,6 @@ public class HomeActivity extends AppCompatActivity {
         }
 
         return ((float) level / (float) scale) * 100.0f;
+
     }
 }
